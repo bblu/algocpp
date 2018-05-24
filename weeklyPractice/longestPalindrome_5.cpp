5742bcd1c7b86d49b1597603fd08ce1366988130
bblu <gmwblu@gmail.com>
2018-05-24 16:48:28 +0800

    weekly

--- weeklyPractice/longestPalindrome_5.cpp
+++ weeklyPractice/longestPalindrome_5.cpp
@@ -2,14 +2,32 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
-#include <vector>
-#include <set>
 
 using namespace std;
 
 class Solution {
 public:
-    string longestPalindrome(string s) {
+    std::string longestPalindrome(std::string s) {
+        if (s.size() < 2)
+            return s;
+        int len = s.size(), max_left = 0, max_len = 1, left, right;
+        for (int start = 0; start < len && len - start > max_len / 2;) {
+            left = right = start;
+            while (right < len - 1 && s[right + 1] == s[right])
+                ++right;
+            start = right + 1;
+            while (right < len - 1 && left > 0 && s[right + 1] == s[left - 1]) {
+                ++right;
+                --left;
+            }
+            if (max_len < right - left + 1) {
+                max_left = left;
+                max_len = right - left + 1;
+            }
+        }
+        return s.substr(max_left, max_len);
+    }
+    string longestPalindrome_old(string s) {
         int size = (int)s.size();
         int curLeft=0,maxLen=1;
         int i,j;
